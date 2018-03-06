5a667b761ccae72e5cd98edc6fe174bace22fbbc
Willem Kowal <willem.kowal@gmail.com>
2018-03-06 13:35:09 -0500

    added fake json

--- Dispatcher/Web/admin/res/PatientDeviceEdit.js
+++ Dispatcher/Web/admin/res/PatientDeviceEdit.js
@@ -3,6 +3,40 @@ var service = 'wherever this is going';
 $(document).ready(function(){
 	//TODO make ajax call to get this device's data, 
 	//then populate all the inputs with the current values
+	
+	$.ajax({
+		type: "GET",
+		url: 'devices/',
+		data: "{}",
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success: function (data) {
+			$.each(data.device, function (k, v) {
+				document.getElementById(k).innerHTML=v;
+			});
+		},
+		error: function (msg) {
+			
+			//Temp JSON
+			msg='{"deviceId":"devId1","deviceType":"autoFallDetection","room":"1234","status":"Storage","username":"aUserName","password":"password1234"}';
+			var jsonMSG=JSON.parse(msg);
+			$.each(jsonMSG, function (k, v) {
+				
+				document.getElementById(k).value=v;
+				
+			/*	if(k == "notificationGroups"){
+					$.each(v, function (num, group){
+					var option = document.createElement("option");
+					option.value=group;
+					option.innerHTML=group;
+					document.getElementById("notificationGroups").appendChild(option);
+					});
+				}else{
+					document.getElementById(k).value=v;
+				}*/
+			});
+		}
+	});
 })
 
 
--- Dispatcher/Web/admin/res/admin.js
+++ Dispatcher/Web/admin/res/admin.js
@@ -3,40 +3,7 @@ var table;
 $(document).ready(function(){
 
 	table = $('#deviceTable').DataTable();
- 
-	$.ajax({
-		type: "GET",
-		url: service + '/devices/',
-		data: "{}",
-		contentType: "application/json; charset=utf-8",
-		dataType: "json",
-		success: function (data) {
-			$.each(data.Countries, function (i, item) {
-				table.row.add([
-				"devId","dev type","dev status","<a href=\"#\" class=\"btn btn-primary\">edit</a>"
-				]).draw( true );
-			});
-					
-		},
-		error: function (msg) {
-			//alert(msg.responseText);
-
-			table.row.add([
-			"devId","dev type1","dev status","<a href=\"PatientDevEdit.html?devId=devId\" class=\"btn btn-primary\">edit</a>"
-			]).draw( true );
-			table.row.add([
-			"devId2","dev type2","dev status","<a href=\"PatientDevEdit.html?devId=devId2\" class=\"btn btn-primary\">edit</a>"
-			]).draw( true );
-			table.row.add([
-			"devId3","dev type3","dev status","<a href=\"PatientDevEdit.html?devId=devId3\" class=\"btn btn-primary\">edit</a>"
-			]).draw( true );
-
-					
-		}
-	});
-	
-	
-	
+	filterTable("inUse");
 	$('#deviceTable tbody').on( 'click', 'tr', function () {
 		if ( $(this).hasClass('selected') ) {
 			$(this).removeClass('selected');
@@ -49,12 +16,45 @@ $(document).ready(function(){
 
 function filterTable(filter){
 
+	if(event){
+			//highlight the clicked button
+	document.getElementById("inUse").className="";
+	document.getElementById("inStorage").className="";
+	document.getElementById("activeAlerts").className="";
+	event.currentTarget.className = "is-active";
+	}
+	
+
+
 	table.clear();
-	
-	//TODO make ajax call to get all devices which match the filter (see onLoad for example)
-	
-	//TODO add all new values to table
-	
+
+	$.ajax({
+		type: "GET",
+		url: 'devices/',
+		data: "{}",
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success: function (data) {
+			$.each(data.device, function (k, v) {
+			table.row.add([
+			"devId","dev type1","dev status","<a href=\"PatientDevEdit.html?devId=devId\" class=\"btn btn-primary\">edit</a>"
+			]).draw();
+			});
+					
+		},
+		error: function (msg) {
+			
+			//Temp JSON
+			msg='{ "deviceArray":[{"deviceId":"dev1","deviceType":"autoFallDetection","deviceStatus":"active"},{"deviceId":"dev2","deviceType":"autoFallDetection","deviceStatus":"active"}]}';
+			var jsonMSG=JSON.parse(msg);
+			//TODO duplicate / move to success block once schema is finalized
+			$.each(jsonMSG.deviceArray, function (deviceArray, device) {
+				table.row.add([
+					device.deviceId,device.deviceType,device.deviceStatus,"<a href=\"PatientDevEdit.html?devId="+device.deviceId+"\" class=\"btn btn-primary\">edit</a>"
+				]);
+			});
+			table.draw();
+		}
+	});
 	table.draw();
-	alert(filter);
 }
