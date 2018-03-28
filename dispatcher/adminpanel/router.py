2e3dc2d1af0d1f050892792e5a4bd800f249f7a5
Rodger <rwald@uocsclub.ca>
2018-03-28 10:21:31 -0400

    Moved files around.

--- dispatcher/adminpanel/router.py
+++ dispatcher/adminpanel/router.py
@@ -7,11 +7,28 @@ from dispatcher.adminpanel import (PanelHandler,
 
 class AdminRouter(SimpleRouter):
     """docstring for NurseRouter"""
+    """docstring for NurseRouter"""
     def __init__(self):
+        print(os.getcwd())
         routes = [
             (r'/admin/', PanelHandler),
             (r'/admin/device', DeviceHandler),
             (r'/admin/devices', DevicesHandler),
             (r'/admin/devicetypes', DeviceTypesHandler),
+            (r'/admin/(favicon.ico)',
+                tornado.web.StaticFileHandler,
+                {'path': 'dispatcher/adminpanel/static/'}),
+            (r'/admin/images/(.*)',
+                tornado.web.StaticFileHandler,
+                {'path': 'dispatcher/adminpanel/static/images/'}),
+            (r'/admin/fonts/(.*)',
+                tornado.web.StaticFileHandler,
+                {'path': 'dispatcher/adminpanel/static/fonts/'}),
+            (r'/admin/css/(.*)',
+                tornado.web.StaticFileHandler,
+                {'path': 'dispatcher/adminpanel/static/css/'}),
+            (r'/admin/js/(.*)',
+                tornado.web.StaticFileHandler,
+                {'path': 'dispatcher/adminpanel/static/js/'})
         ]
         super(AdminRouter, self).__init__(routes)
